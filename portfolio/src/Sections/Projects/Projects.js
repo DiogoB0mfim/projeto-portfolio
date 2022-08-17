80d8e929c611b05a4e54e92effe2e94758b7f02b
DiogoB0mfim <diogohbomfim@gmail.com>
2022-08-17 18:44:05 -0300

    content hover adicionado

--- portfolio/src/Sections/Projects/Projects.js
+++ portfolio/src/Sections/Projects/Projects.js
@@ -4,16 +4,19 @@ import projectThree from "../../Assets/projectPokedex.png"
 import projectTwo from "../../Assets/projectFoodJP.png"
 import github from "../../Assets/github.png"
 import deploy from "../../Assets/deploy.png"
+import { useState } from "react"
 // Import Swiper 
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
-import "swiper/css/pagination";
 import { FreeMode} from "swiper";
 
+
 const Projects = () => {
+    const [descriptionP1, setDescriptionP1] = useState("")
+    const [descriptionP2, setDescriptionP2] = useState("")
+    const [descriptionP3, setDescriptionP3] = useState("")
+    
     return (
         <div className="div-projects-section">
             <div className="title">
@@ -42,9 +45,16 @@ const Projects = () => {
                     pagination={{clickable: true,}}
                     modules={[FreeMode]}
                     className="mySwiper">
+                        
                     <SwiperSlide>
                         <div className="project">
-                            <img src={projectOne}/>
+                            <img 
+                                src={projectOne}
+                                onMouseOver={() => 
+                                    setDescriptionP1("O Rappi4, foi proposto pela Labenu com intenção de colocar em prática o conhecimento do módulo front-end. Tem o intuito de simular uma aplicação web para pedidos.")}
+                                onMouseOut={() => setDescriptionP1("")}
+                                />
+                            <p>{descriptionP1}</p>
                             <div className="container-btn">
                                 <button className="btn-git"><img src={github}/>GitHub</button>
                                 <button className="btn-deploy"><img src={deploy}/>Visualizar</button>
@@ -53,7 +63,12 @@ const Projects = () => {
                     </SwiperSlide>
                     <SwiperSlide>
                         <div className="project">
-                            <img src={projectTwo}/>
+                            <img src={projectTwo}
+                            onMouseOver={() => 
+                                setDescriptionP2("O FoodJP é um projeto do desafio nº27 da comunidade Codelandia, com o design desenvolvido pelo Iuri Silva. Utilizei deste desafio para consolidar meu conhecimento em HTML, CSS e JS.")}
+                            onMouseOut={() => setDescriptionP2("")}
+                            />
+                            <p>{descriptionP2}</p>
                             <div className="container-btn">
                                 <button className="btn-git"><img src={github}/>GitHub</button>
                                 <button className="btn-deploy"><img src={deploy}/>Visualizar</button>
@@ -62,7 +77,12 @@ const Projects = () => {
                     </SwiperSlide>
                     <SwiperSlide>
                         <div className="project">
-                            <img src={projectThree}/>
+                            <img src={projectThree}
+                             onMouseOver={() => 
+                                setDescriptionP3("O Pokédex foi feito com o intuito de simular um site com mais de 1000 pokémons extraidos da PokéAPI e com algumas funcionalidades, dentre elas adicionar, excluir e ver detalhes dos pokémons.")}
+                            onMouseOut={() => setDescriptionP3("")}
+                            />
+                            <p>{descriptionP3}</p>
                             <div className="container-btn">
                                 <button className="btn-git"><img src={github}/>GitHub</button>
                                 <button className="btn-deploy"><img src={deploy}/>Visualizar</button>
